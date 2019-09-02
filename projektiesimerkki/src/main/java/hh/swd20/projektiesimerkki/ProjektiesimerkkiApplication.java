36df88979c014e519e8791d04f5ce06441e6be54
unknown <silvasiven@gmail.com>
2019-09-02 10:13:00 +0300

    eka lisäys

---
+++ projektiesimerkki/src/main/java/hh/swd20/projektiesimerkki/ProjektiesimerkkiApplication.java
@@ -0,0 +1,13 @@
+package hh.swd20.projektiesimerkki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektiesimerkkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektiesimerkkiApplication.class, args);
+	}
+
+}
